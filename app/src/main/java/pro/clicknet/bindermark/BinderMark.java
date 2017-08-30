f6600b77cc06a12fc37b72790d6d1cbbf204326f
andixlm <andixev@gmail.com>
2017-08-30 22:51:00 +0300

    app: Initial commit

---
+++ app/src/main/java/pro/clicknet/bindermark/BinderMark.java
@@ -0,0 +1,13 @@
+package pro.clicknet.bindermark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BinderMark extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
