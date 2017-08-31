606a13447307a78e5af8414e0fff05121c9d3645
andixlm <andixev@gmail.com>
2017-08-31 13:06:56 +0300

    app: Implement UI

--- app/src/main/java/pro/clicknet/bindermark/BinderMark.java
+++ app/src/main/java/pro/clicknet/bindermark/BinderMark.java
@@ -8,6 +8,8 @@ public class BinderMark extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.bindermark);
     }
 
 }
