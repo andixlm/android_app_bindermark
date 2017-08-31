f8640252b90dac0d1f3880fd879e8bb16608a8fc
andixlm <andixev@gmail.com>
2017-08-31 10:30:43 +0300

    lib: common: Implement BMResponse

---
+++ common/src/main/aidl/pro/clicknet/bindermarkcommon/BMResponse.aidl
@@ -0,0 +1,3 @@
+package pro.clicknet.bindermarkcommon;
+
+parcelable BMResponse;
---
+++ common/src/main/java/pro/clicknet/bindermarkcommon/BMResponse.java
@@ -0,0 +1,47 @@
+package pro.clicknet.bindermarkcommon;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class BMResponse implements Parcelable {
+
+    private long mReceiptTime;
+
+    public BMResponse(long receiptTime) {
+        setReceiptTime(receiptTime);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(mReceiptTime);
+    }
+
+    public static final Creator<BMResponse> CREATOR =
+            new Creator<BMResponse>() {
+
+                @Override
+                public BMResponse createFromParcel(Parcel in) {
+                    return new BMResponse(in.readLong());
+                }
+
+                @Override
+                public BMResponse[] newArray(int size) {
+                    return new BMResponse[size];
+                }
+
+            };
+
+    public long getReceiptTime() {
+        return mReceiptTime;
+    }
+
+    public void setReceiptTime(long receiptTime) {
+        mReceiptTime = receiptTime;
+    }
+
+}
