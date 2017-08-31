96f2836f12f0a3876469af4e3e8c74a4a5dbcfa8
andixlm <andixev@gmail.com>
2017-08-31 11:24:40 +0300

    lib: client: BMClientService: Store server

--- client/src/main/java/pro/clicknet/bindermarkclientservice/BMClientService.java
+++ client/src/main/java/pro/clicknet/bindermarkclientservice/BMClientService.java
@@ -4,10 +4,19 @@ import android.os.RemoteException;
 
 import pro.clicknet.bindermarkcommon.BMResponse;
 import pro.clicknet.bindermarkcommon.IBinderMarkClientService;
+import pro.clicknet.bindermarkserverservice.BMServerService;
 
 public class BMClientService extends IBinderMarkClientService.Stub {
 
-    // TODO: Store server.
+    private BMServerService mServer;
+
+    public BMClientService(BMServerService server) throws IllegalArgumentException {
+        if (server == null) {
+            throw new IllegalArgumentException("Server must be non-null");
+        }
+
+        mServer = server;
+    }
 
     @Override
     public BMResponse perform(int size) throws RemoteException {
