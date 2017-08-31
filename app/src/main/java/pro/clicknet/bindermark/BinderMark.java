174fe35a7739a461b11ad196719165470a0f438b
andixlm <andixev@gmail.com>
2017-08-31 13:12:15 +0300

    app: BinderMark: Store UI elements

--- app/src/main/java/pro/clicknet/bindermark/BinderMark.java
+++ app/src/main/java/pro/clicknet/bindermark/BinderMark.java
@@ -2,12 +2,35 @@ package pro.clicknet.bindermark;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class BinderMark extends Activity {
+
+    private static final int DEFAULT_SIZE = 1;
+    private static final boolean DEFAULT_NATIVE_METHOD = false;
+
+    private int mSize;
+    private EditText mSizeText;
+
+    private boolean mNativeMethod;
+    private Switch mNativeMethodSwitch;
+
+    private Button mPerformButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSize = DEFAULT_SIZE;
+        mSizeText = (EditText) findViewById(R.id.text_size);
+        mSizeText.setText(String.valueOf(mSize));
+
+        mNativeMethod = DEFAULT_NATIVE_METHOD;
+        mNativeMethodSwitch = (Switch) findViewById(R.id.switch_native_method);
+
+        mPerformButton = (Button) findViewById(R.id.button_perform_test);
 
         setContentView(R.layout.bindermark);
     }
