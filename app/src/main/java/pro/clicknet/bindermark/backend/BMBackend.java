b8461d29795f37bca3f860cd5793c657a807daf0
andixlm <andixev@gmail.com>
2017-09-01 09:09:43 +0300

    app: backend: Store size

--- app/src/main/java/pro/clicknet/bindermark/backend/BMBackend.java
+++ app/src/main/java/pro/clicknet/bindermark/backend/BMBackend.java
@@ -4,18 +4,34 @@ import pro.clicknet.bindermarkcommon.BMResponse;
 
 public class BMBackend {
 
+    private static final int MINIMUM_SIZE = 0;
+    private static final int MAXIMUM_SIZE = 512;
+
+    // Size must be set to positive value before performing test.
+    private static final int DEFAULT_SIZE = -1;
     private static final boolean DEFAULT_NATIVE_METHOD = false;
 
+    private int mSize;
     private boolean mNativeMethod;
 
     private OnCompleteListener mOnCompleteListener;
 
     public BMBackend() {
+        mSize = DEFAULT_SIZE;
         mNativeMethod = DEFAULT_NATIVE_METHOD;
     }
 
-    public BMBackend(boolean nativeMethod) {
+    public BMBackend(int size, boolean nativeMethod) {
+        setSize(size);
         setNativeMethod(nativeMethod);
+    }
+
+    public int getSize() {
+        return mSize;
+    }
+
+    public void setSize(int size) {
+        mSize = size;
     }
 
     public boolean getNativeMethod() {
