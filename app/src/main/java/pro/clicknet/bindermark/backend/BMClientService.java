e99d74c921bcf72fc6b64f53f0a1a5a6398f7257
andixlm <andixev@gmail.com>
2017-09-02 14:29:05 +0300

    app: backend: Implement BMClientService

---
+++ app/src/main/java/pro/clicknet/bindermark/backend/BMClientService.java
@@ -0,0 +1,34 @@
+package pro.clicknet.bindermark.backend;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import pro.clicknet.bindermarkclientservice.IBMClientServiceImpl;
+
+public class BMClientService extends Service {
+
+    private IBMClientServiceImpl mService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mService = new IBMClientServiceImpl();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mService = null;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mService;
+    }
+
+}
